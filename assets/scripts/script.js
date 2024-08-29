ec437c32162d6f279b9f9d9d482ff55627c14180
a-abdurrahman <aa.abdurrahman03@gmail.com>
2024-08-29 18:40:30 +0700

    First Initialization

---
+++ assets/scripts/script.js
@@ -0,0 +1,6 @@
+function toggleTable() {
+    document.getElementById("info-table").classList.toggle("hide")
+    document.getElementById("table-hide").classList.toggle("hide")
+    document.getElementById("table-show").classList.toggle("hide")
+}
+
